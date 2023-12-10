b3a7ed52a458098b85c6c82461b7fc97ef934df8
Thanh Lưu Gia <thanhlg@hqsoft.com.vn>
2023-12-10 23:14:03 +0700

    update template

---
+++ OnlineSchool_Project/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineSchool_Project.Data;
+
+namespace OnlineSchool_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        public IActionResult Dangnhap()
+        {
+            return View();
+        }
+         
+    }
+}
