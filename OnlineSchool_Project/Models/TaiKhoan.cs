6af9ead813977402c27f491458c53e2692826ccb
Jiaqing <123981771+giathanhit@users.noreply.github.com>
2023-12-10 14:08:14 +0700

    Update TaiKhoan.cs

--- OnlineSchool_Project/Models/TaiKhoan.cs
+++ OnlineSchool_Project/Models/TaiKhoan.cs
@@ -2,7 +2,6 @@
 {
     public class TaiKhoan
     {
-        public int Id { get; set; }
         public string TenDangNhap { get; set; } = null!;
         public string MatKhau { get; set; } = null!; 
     }
